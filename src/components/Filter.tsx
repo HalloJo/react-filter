ac390ce0b3b6a49525f58a8bb1721aaa509ea50e
Jorik van Ruiswijk <jorikvanruiswijk@gmail.com>
2024-05-23 20:13:48 +0200

    Change title of page and fix typing

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { food } from '../data/food';
+import React from "react";
+import { Food, food } from "../data/food";
 
 type MenuProps = {
   menuCategories: string[];
   filterItems: (category: string) => void;
-  setItems: (items: any) => void;
+  setItems: (items: Food[]) => void;
 };
 
 const Filter = ({ menuCategories, filterItems, setItems }: MenuProps) => {
